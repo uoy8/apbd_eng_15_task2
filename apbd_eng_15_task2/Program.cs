00fe4d932b7fa7c795ecda777bd39e2bb7087029
uoy8 <mrtwaterr@gmail.com>
2026-03-12 17:13:24 +0200

    input error message added

--- apbd_eng_15_task2/Program.cs
+++ apbd_eng_15_task2/Program.cs
@@ -1,3 +1,9 @@
 ﻿Console.WriteLine("Enter numbers separated by space:");
 string input = Console.ReadLine();
 int[] numbers = input.Split(' ').Select(int.Parse).ToArray();
+
+if (numbers.Length == 0)
+{
+    Console.WriteLine("Error: No numbers provided.");
+    return;
+}
